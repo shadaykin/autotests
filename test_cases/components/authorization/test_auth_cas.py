86a3ce83dc57d4171891669e3691b2defe20e766
Садретдинов Ильдар <isadretdinov@rutube.corp>
2019-06-27 11:58:23 +0300

    27.06

---
+++ test_cases/components/authorization/test_auth_cas.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time, os, json, sys
+from functions.clear_cash import Cookie
+from functions.authorization import Authorization
+
+
+def test_auth_success():
+    cash = Cookie.clear_cash_func()
+    driver = webdriver.Firefox(cash)
+
+    # driver.maximize_window()
+    try:
+        Authorization.correct_authorization(driver)
+        assert "accounts/edit" == driver.current_url.split("https://passport.jw-test.zxz.su/")[1], driver.close()
+        driver.close()
+    except:
+        assert 1 == 2, driver.close()
+
+
