669e7ea22de57b70e0885b64a1fa0d00fc2a8f46
isadretdinov <blazer.3@yandex.ru>
2019-10-24 17:05:24 +0300

    done taking session_id with requests module

--- pass_media/test_cases/functions/session_id_2.py
+++ pass_media/test_cases/functions/session_id_2.py
@@ -1,11 +1,20 @@
 import requests
-import enviroments
+
 def get_sessionid():
     link = "https://passport.jw-test.zxz.su/cas/login"
-    req = requests.get(link)
-    csrftoken=req.cookies['csrftoken']
-    payload = {'username': enviroments.options['phone'], 'password': enviroments.options['password']}
 
+    s = requests.Session()
+    get_csrf = s.get(link)
+    csrftoken = get_csrf.cookies['csrftoken']
+    cookies = {'csrftoken': csrftoken}
+    headers = {
+        'X-CSRFToken': csrftoken, 'Referer': link
+    }
+    files = {'username': (None, '+79096201687'), 'password': (None, '111111xX')}
+
+    response = s.post(link, files=files, headers=headers, cookies=cookies)
+
+    print(s.cookies.get_dict())
 
 
 
