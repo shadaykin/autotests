f5d38f36aa48c241ae233f0f09c08190c186a317
shadaykin <blazer.3@yandex.ru>
2020-09-08 14:13:01 +0300

    add refunds test

--- pass_media/for_me.py
+++ pass_media/for_me.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, random
 import pytest
+import sys
 
 
-a = False
-b = 0
-while not a and b < 2:
-    b += 1
-    a = True
+tokens = dict(
+    test_201='2efe49abccad96de57e7c96923eccf90f6b1886c',
+    other_201='740ea2823503035e6fdef524db3ad81f77ea705c')
 
+env = 'test_201'
 
-print(a)
+header = tokens['other'+env.split('test')[1]]
+
+print(header)
